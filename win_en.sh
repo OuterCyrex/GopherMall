078018f169b1ccbc9dc7face306a23f721b175cb
OuterCyrex <a15839962056@outlook.com>
2024-12-30 21:27:51 +0800

    update:load_balance

---
+++ win_en.sh
@@ -0,0 +1,7 @@
+powershell -Command "
+Start-Process powershell \"-Command Start-Process -FilePath 'E:\DevEnvironment\Redis\redis-server.exe'\"
+
+Start-Process powershell -ArgumentList \"-Command Set-Location -Path 'E:\DevEnvironment\Nacos\nacos\bin';.\startup.cmd -m standalone\"  -WindowStyle Normal
+
+Start-Process powershell -ArgumentList \"-Command Set-Location -Path 'E:\DevEnvironment\Consul';.\consul agent -dev\" -WindowStyle Normal
+"
